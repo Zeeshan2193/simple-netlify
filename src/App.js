c7c7fe8e82603b46cdb95154fb905b78d304ec95
Zeeshan Ali <zeeshan2193@gmail.com>
2021-01-03 21:23:51 +0500

    New Update
    New update is here

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello From Zeeshan</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h1>Hello From Zeeshan Ali</h1>
+        </a>
       </header>
     </div>
   );
