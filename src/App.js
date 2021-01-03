74433272925303226c609217b57eea89d68570c2
Zeeshan Ali <zeeshan2193@gmail.com>
2021-01-03 21:19:54 +0500

    Update Name
    Update my name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello From Zeeshan Ali Rajpot</h1>
+        <h1>Hello From Zeeshan</h1>
       </header>
     </div>
   );
